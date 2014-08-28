4fe9b17849d280de9375fd4e849b2548e6def0f5
Dwight Townsend <dwtownse@calpoly.edu>
2014-08-28 12:08:14 -0700

    Model and Controller

---
+++ blog.py
@@ -0,0 +1,22 @@
+# blog.py - controller
+
+
+# imports
+from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
+import sqlite3
+
+# configuration
+DATABASE = 'blog.db'
+
+app = Flask(__name__)
+
+# pulls in app configuration by looking for UPPERCASE varibles
+app.config.from_object(__name__)
+
+# function used for db connection
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
